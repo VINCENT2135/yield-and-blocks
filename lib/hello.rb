0971ca93300055d7576b9cdb3d19183408cfde07
Vincent Ivos <vincentivos1@gmail.com>
2020-11-15 19:21:21 -0500

    commit tree f6fbf6c5db5d995beb2299939240ee1c46de626a

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,24 @@
-def hello_t
+def hello_t(array)
+    if block_given?
+      i = 0
+  
+      while i < array.length
+        yield(array[i])
+        i = i + 1
+      end
+  
+      array
+    else
+      puts "Hey! No block was given!"
+    end
+  end 
 
-end
-
+  hello_t (["Tim", "Tom", "Jim"]) do |element|
+    if element.start_with?("T")
+        puts "Hi, #{element}"
+    end 
+    end 
+    
 # call your method here!
+
 
